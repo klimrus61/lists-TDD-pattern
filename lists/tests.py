fd728eb2fb88eeab591de48f56d35cfdaf0b51ab
klimrus61 <klimrus61@mail.ru>
2022-10-30 19:55:24 +0300

    The list application was added using a specially broken unit test

---
+++ lists/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+# Create your tests here.
+class SmokeTest(TestCase):
+    """тест на токсичность"""
+
+    def test_bad_maths(self):
+        '''Тест: неправильные математические расчеты'''
+        self.assertEqual(1 + 1, 3)

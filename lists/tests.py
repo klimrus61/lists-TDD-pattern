ccb6d2a4c895fe884e06e05dfe912aa45c8717be
klimrus61 <klimrus61@mail.ru>
2022-10-30 20:33:10 +0300

    First unit test and url conversion, fictitious representation

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
-class SmokeTest(TestCase):
-    """тест на токсичность"""
 
-    def test_bad_maths(self):
-        '''Тест: неправильные математические расчеты'''
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    """Тест домашней страницы"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """тест: корневой url преобразуется в представление домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)

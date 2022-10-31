c1a84ff61b0d03a555cc29791279c82a9856f4e4
klimrus61 <klimrus61@mail.ru>
2022-10-31 21:34:22 +0300

    HTML main page now generates from template

--- functional_tests.py
+++ functional_tests.py
@@ -25,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-do', header_text)
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,7 +46,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == "1: Купить павлиньи перья" for row in rows)
+            any(row.text == "1: Купить павлиньи перья" for row in rows),
+            "Новый элемент списка не появился в таблице"
         )
 
         # Текстовое поле по-прежнему приглашает ее добавить еще 1 элемент.

55bb53dda1ccd24a9a48706e0beb1e76fef74da6
klimrus61 <klimrus61@mail.ru>
2022-10-31 21:16:09 +0300

    Refactored home page views to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -8,16 +8,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """тест: корневой url преобразуется в представление домашней страницы"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        """тест: домашняя станица возвращает правильный html"""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        """тест: используется домашний шаблон"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')

7d0d903b7148fc2aad8230746fa741d84f1be31d
klimrus61 <klimrus61@mail.ru>
2022-11-03 11:29:27 +0300

    функциональные тесты проверяют 2 элемента в списке, код требует реф

--- functional_tests.py
+++ functional_tests.py
@@ -41,30 +41,38 @@ class NewVisitorTest(unittest.TestCase):
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == "1: Купить павлиньи перья" for row in rows),
-            "Новый элемент списка не появился в таблице"
-        )
+        self.assertIn("1: Купить павлиньи перья", [row.text for row in rows])
 
         # Текстовое поле по-прежнему приглашает ее добавить еще 1 элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Соня очень методична)
-        self.fail("Закончить тест!")
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+        
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn(
+            '2: Сделать мушку из павлиньих перьев', 
+            [row.text for row in rows]
+        )
 
         # Соне интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальныйы URL-адресс - об этом
         # выводится небольшой текст с обьяснениями.
+        self.fail('Закончить тест!')
 
         # Она посещает этот URL-адрес - ее список по-прежнему там.
 
         # Удовлетворенная, она снова ложится спать
-        browser.quit()
+
         
 if __name__ == '__main__':
     unittest.main()

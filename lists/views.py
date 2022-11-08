24817296b66d1d5b76b1ee71990fbd6435689931
klimrus61 <klimrus61@mail.ru>
2022-11-08 19:48:24 +0300

    URL-адреса для 2 форм, перем-н код в views + тесты, новый URL адрес

--- lists/views.py
+++ lists/views.py
@@ -4,14 +4,14 @@ from lists.models import Item
 
 def home_page(request):
     """Домашняя страница"""
-    if request.method == "POST":
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/one-single-list-of-the-world/')
-    
     return render(request, 'home.html')
 
 def view_list(request):
     '''представление списка'''
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
-    
+
+def new_list(request):
+    '''новый список'''
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/one-single-list-of-the-world/')
--- superlists/settings.py
+++ superlists/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Убираю обязательный слеш в конце для POST
+APPEND_SLASH = False

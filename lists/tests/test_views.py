0cefff4e89ae486b4b42745e4c8b907d131e6a7a
klimrus61 <klimrus61@mail.ru>
2022-12-05 14:58:05 +0300

    Рефракторизовано представление списка для обработки POST запросов с новыми элементами

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -44,32 +44,13 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
-class NewListTest(TestCase):
-    '''тест нового списка'''
-
-    def test_can_save_a_POST_request(self):
-        """тест: можно сохранить POST запрос"""
-        self.client.post('/lists/new', data={"item_text": 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-    
-    def test_redirects_after_POST(self):
-        '''тест: переадресует после post-запроса'''
-        response = self.client.post('/lists/new', data={"item_text": 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
-
-class NewItemTest(TestCase):
-    '''тест: нового элемента списка'''
-
     def test_can_save_a_POST_request_to_an_existing_list(self):
         '''тест: может сохранить post-запрос в существующий список'''
         other_list = List.objects.create()
         corrent_list = List.objects.create()
 
         self.client.post(
-            f'/lists/{corrent_list.id}/add_item',
+            f'/lists/{corrent_list.id}/',
             data={'item_text': 'A new item for an existing list'},
         )
 
@@ -78,13 +59,13 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, corrent_list)
     
-    def test_redirects_to_list_view(self):
-        '''тест: переадресуется в представление списка'''
+    def test_POST_redirects_to_list_view(self):
+        '''тест:post-запрос переадресуется в представление списка'''
         other_list = List.objects.create()
         corrent_list = List.objects.create()
 
         response = self.client.post(
-            f'/lists/{corrent_list.id}/add_item',
+            f'/lists/{corrent_list.id}/',
             data={'item_text': 'A new item for an existing list'},
         )
 
@@ -104,3 +85,19 @@ class NewItemTest(TestCase):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+class NewListTest(TestCase):
+    '''тест нового списка'''
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить POST запрос"""
+        self.client.post('/lists/new', data={"item_text": 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+    
+    def test_redirects_after_POST(self):
+        '''тест: переадресует после post-запроса'''
+        response = self.client.post('/lists/new', data={"item_text": 'A new list item'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')

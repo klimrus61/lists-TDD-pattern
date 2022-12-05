29d2100aea153ae6bb7b239c6e802bcea7b51b0f
klimrus61 <klimrus61@mail.ru>
2022-12-05 14:33:17 +0300

    Скоректированно новое представление списка с учетом валидации модели

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.html import escape
 from lists.models import Item, List
 
 
@@ -88,3 +89,18 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{corrent_list.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        '''тест: ошибки валидации отсылаются назад в шаблон
+            домашней страницы'''
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        '''тест: недопустимые элементы списка не сохраняются'''
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)

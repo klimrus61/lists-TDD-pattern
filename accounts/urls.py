0ef96f89fd0acafb098413fbab062616c012fa93
klimrus61 <klimrus61@mail.ru>
2022-12-22 13:20:42 +0300

    Добавлена возможность выхода из системы, покрыта ФТ

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from accounts import views
+from django.contrib.auth.views import logout_then_login
 
 
 urlpatterns = [
     path('send_login_email/', views.send_login_email, name='send_login_email'),
     path('login/', views.login, name='login'),
+    path('logout/', logout_then_login, name='logout')
 ]
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -51,3 +51,13 @@ class LoginTest(FunctionalTest):
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
+
+        # Теперь она выходит из системы
+        self.browser.find_element_by_link_text('Log out').click()
+
+        # Вышла из системы
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(TEST_EMAIL, navbar.text)

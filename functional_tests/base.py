f84ffbd60b8691dd041228c011d076c69a4da1b6
klimrus61 <klimrus61@mail.ru>
2022-12-23 13:30:02 +0300

    test_my_lists: пересоздание сеансов, перемещение проверок регистрации в базу

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -53,3 +53,20 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         '''получить поля ввода для элемента'''
         return self.browser.find_element_by_id('id_text')
+
+    def wait_to_be_logged_in(self, email):
+        '''ожидать входа в систему'''
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        '''ожидать выхода из системы'''
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
+        
